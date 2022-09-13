76d3d317f34b5658d5f11a0669cfbfebcf612315
tluciad <tluciad@gmail.com>
2022-09-13 10:39:18 -0500

    render template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,5 +40,10 @@ def display_html(n):
     return render_template("5-number.html", n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def display_odd_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
