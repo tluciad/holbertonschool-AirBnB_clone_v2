526c4ef35f560cd842a7e5f9b22c3359784ff04e
tluciad <tluciad@gmail.com>
2022-09-14 15:00:40 -0500

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,19 +3,19 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def display_cities():
-    states = storage.all("State").values
+    states = storage.all(State).values()
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(self):
     return storage.close()
 
 
